97211d6056920040de52d1cdde7d040e0d4a68f1
Pavlos <pavlosbatumi@gmail.com>
2026-08-04 16:51:27 +0200

    Add image field to Tip model

--- backend/apps/content/models.py
+++ backend/apps/content/models.py
@@ -5,6 +5,7 @@ from django.utils.translation import gettext_lazy as _
 class Tip(models.Model):
     title = models.CharField(max_length=200, verbose_name=_('Заголовок'))
     body = models.TextField(verbose_name=_('Текст'))
+    image = models.ImageField(upload_to='tips/', blank=True, verbose_name=_('Картинка'))
     tags = models.CharField(max_length=255, blank=True, verbose_name=_('Теги через запятую'))
     order = models.PositiveIntegerField(default=0, verbose_name=_('Порядок'))
     is_active = models.BooleanField(default=True, verbose_name=_('Активен'))
