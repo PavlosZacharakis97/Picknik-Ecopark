0933c59b50c3212d887567edfe27317a2d79192d
Pavlos <pavlosbatumi@gmail.com>
2026-08-05 11:33:57 +0200

    Toggle navbar scrolled state on scroll

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,3 +1,8 @@
 // точка вчщда в апликацию
 // router.js автоматически инициализируется при загрузке страницы
 // Все скрипты в index.html
+
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (navbar) navbar.classList.toggle('scrolled', window.scrollY > 10);
+});
